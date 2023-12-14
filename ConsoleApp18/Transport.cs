f632a39863541242f30497dbbec81381fa851461
Globlobglob <122725189+Globlobglob@users.noreply.github.com>
2023-12-14 11:31:40 +0200

    Update Transport.cs

--- ConsoleApp18/Transport.cs
+++ ConsoleApp18/Transport.cs
@@ -8,5 +8,35 @@ namespace ConsoleApp18
 {
     class Transport
     {
+        
+        public string tip;
+        public enum Color
+        {
+            Green, Blue, GreenYellow, GreenGreen
+        }
+        
+        public int speed;
+        public int mase;
+        
+
+        public Color color;
+
+
+        public Class1() { }
+
+        public Class1(string tip,Color color,int speed,int mase) 
+        {
+            this.tip = tip;
+            this.color = color;
+            this.speed = speed;
+            this.mase = mase;
+
+        
+        }
+
+        public override string ToString()
+        {
+            return $"{tip}\n{color}\n{speed}\n{mase}";
+        }
     }
 }
